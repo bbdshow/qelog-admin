5b6cfd549fd95ce5deb7a14f2c76dbd067f2532e
zhongqing2016 <zhongqing2016@qq.com>
2021-01-06 23:28:17 +0800

    删除多余文件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -31,9 +31,6 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
-          <router-link to="/">
-            <el-dropdown-item>Logging</el-dropdown-item>
-          </router-link>
           <el-dropdown-item divided @click.native="logout">
             <span style="display: block">Log Out</span>
           </el-dropdown-item>
