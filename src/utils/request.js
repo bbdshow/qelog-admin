a291727a31ec5c069fb5f8257c24f27d084f2e4c
胡忠庆 <zhongqing2016@qq.com>
2021-01-08 14:21:33 +0800

    权限，替换ico

--- src/utils/request.js
+++ src/utils/request.js
@@ -24,7 +24,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['AccessToken-Token'] = getToken()
+      config.headers['X-QELOG-Token'] = getToken()
     }
     return config
   },
@@ -60,9 +60,9 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 401) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm(`${res.message} 您即将退出，可选择重新登录`, '确认退出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
